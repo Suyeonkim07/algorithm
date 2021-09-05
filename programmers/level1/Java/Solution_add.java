7ffc780b5ef130fb8d13026bbfdce386d7d1eb9f
Suyeonkim07 <ksy4836@gmail.com>
2021-09-06 00:14:29 +0900

    행렬의 덧셈

---
+++ programmers/level1/Java/Solution_add.java
@@ -0,0 +1,15 @@
+//행렬의 덧셈
+
+class Solution {
+    public int[][] solution(int[][] arr1, int[][] arr2) {
+        int row = arr1.length;
+        int col = arr1[0].length;
+        int[][] answer = new int[row][col];
+        for(int a=0; a < row; a++){
+            for(int b=0; b < col; b++){
+                answer[a][b] = arr1[a][b] + arr2[a][b];
+            }
+        }
+        return answer;
+    }
+}
