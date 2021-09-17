778714ac5968223437c34bb46fc060409eafd21a
Suyeonkim07 <ksy4836@gmail.com>
2021-09-17 22:44:39 +0900

    프로그래머스레벨1

---
+++ programmers/level1/Java/hasshad_num.java
@@ -0,0 +1,18 @@
+
+//하샤드 수
+
+class Solution {
+    public boolean solution(int x) {
+        boolean answer = true;
+        int n = x;
+        int num = 0;
+        while(n != 0){
+            num += n%10;
+            n = n/10;
+        }
+        if(x%num == 0)
+            return true;
+        else
+            return false;
+    }
+}
