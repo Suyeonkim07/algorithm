81cce84fb9f3ffae0102e4d434a0afc5c014a4cb
Suyeonkim07 <ksy4836@gmail.com>
2021-10-01 02:12:02 +0900

    짝수와 홀수

---
+++ programmers/level1/Java/EvenOdd.java
@@ -0,0 +1,17 @@
+
+//짝수와 홀수
+
+class Solution {
+    public String solution(int num) {
+        String answer = "";
+        if(num < 0)
+            num *= -1;
+        if (num % 2 >= 1){
+            answer = "Odd";
+        }
+        else
+            answer = "Even";
+        return answer;
+    }
+    
+}
