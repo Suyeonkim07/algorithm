98ff0094dc759b2bd8318bc6cb112752fd389b30
Suyeonkim07 <ksy4836@gmail.com>
2021-10-18 00:27:15 +0900

    정수 제곱근 판별

---
+++ programmers/level1/Java/Description.java
@@ -0,0 +1,17 @@
+
+//정수 제곱근 판별
+
+class Solution {
+    public long solution(long n) {
+        long answer = 0;
+        long x = 0;
+        while(x <= n){
+            if (n == x * x){
+                answer = (x+1)*(x+1);
+                return answer;
+            }
+            x++;
+        }
+        return -1;
+    }
+}
